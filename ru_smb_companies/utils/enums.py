de13b5073145b63b5c5342373cb78c5e8c0509ef
Pavel Syomin <pas.ntg@gmail.com>
2024-01-19 17:58:31 +0500

    Move enums to separate file

---
+++ ru_smb_companies/utils/enums.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class SourceDatasets(enum.Enum):
+    smb = "smb"
+    revexp = "revexp"
+    empl = "empl"
+
+
+class StageNames(enum.Enum):
+    download = "download"
+    extract = "extract"
+    aggregate = "aggregate"
+    georeference = "georeference"
+    panelize = "panelize"
+
+
+class Storages(enum.Enum):
+    local = "local"
+    ydisk = "ydisk"
